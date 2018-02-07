1145e108f125ba2b556c7023a1c75d4f2617a009
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 23:45:21 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data.each do |seasons, contestants|
+    if seasons == season
+      contestants.each do |contestant|
+        if contestant["status"] == "Winner"
+          winner = contestant["name"].split().shift()
+        end 
+      end 
+    end 
+  end 
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  person = ""
+  data.each do |seasons, contestants|
+      contestants.each do |contestant|
+        if contestant["occupation"] == occupation
+          person = contestant["name"]
+        end 
+      end 
+  end 
+  person
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |seasons, contestants|
+      contestants.each do |contestant|
+        if contestant["hometown"] == hometown
+          counter += 1
+        end 
+      end 
+  end 
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = ""
+  data.each do |seasons, contestants|
+      contestants.each do |contestant|
+        if contestant["hometown"] == hometown
+          occupation = contestant["occupation"]
+          break
+        end 
+      end 
+  end 
+  occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  totalage = 0 
+  counter = 0 
+  average = 0 
+  data.each do |seasons, contestants|
+    if seasons == season
+      contestants.each do |contestant|
+          totalage += contestant["age"].to_f
+          counter += 1
+      end 
+    end 
+  end 
+  average = (totalage/counter).round
 end
